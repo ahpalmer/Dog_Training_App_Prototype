801a855b6637de112a04c13391419d97a953c77f
Andrew Palmer <ahpalmer72@gmail.com>
2022-06-03 09:44:45 -0400

    Continuing to implement MVVM
    Updated main window and continued to build profile class

--- Model/User_Profile.cs
+++ Model/User_Profile.cs
@@ -15,22 +15,39 @@ namespace Dog_Training_App_Prototype
 
     internal class User_Profile
     {
-        public string Username;
-        public string Password;
+        private string username;
+        private string password;
         skill_identifier sit = new skill_identifier();
         skill_identifier lay_down = new skill_identifier();
         skill_identifier come = new skill_identifier();
 
         User_Profile(string user, string pass)
         {
-            Username = user;
-            Password = pass;
+            username = user;
+            password = pass;
             sit.Name = "Sit";
             sit.skill_selected = false;
             lay_down.Name = "Lay Down";
             lay_down.skill_selected = false;
             come.Name = "Come";
             come.skill_selected = false;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set {  password = value; }
+        }
+
+        public void set_sit_bool(bool sit)
+        {
+            this.sit.skill_selected = sit;
         }
 
     }
